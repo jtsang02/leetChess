02b3fb8c1eebcd04bdf6c331b7847b34aed225b6
jtsang02 <jtsang02@student.ubc.ca>
2023-01-20 16:36:23 -0800

    draw.io and board state matrix

--- Arduino/reedSwitches_nxn/reedSwitches_nxn.ino
+++ Arduino/reedSwitches_nxn/reedSwitches_nxn.ino
@@ -5,14 +5,38 @@
 */
 
 #include <Adafruit_NeoPixel.h>
-#define BOARD_SIZE 2                  // number of columns and rows in the board
-#define N_LEDS 8 * 8                  // number of individual LEDs in one neopixel strip
-#define BOARD_PIN 12                  // pin for the neopixel strip
-int columnPins[] = { 8, 9, 11, 12 };  // pins for columns
-int rowPins[] = {2, 3, 6, 7 };        // pins for rows
+#define BOARD_SIZE 8               // number of columns and rows in the board
+#define N_LEDS 8 * 8               // number of individual LEDs in one neopixel strip
+#define BOARD_PIN 12               // pin for the neopixel strip
+int columnPins[] = {8, 9, 10, 11}; // pins for columns
+int rowPins[] = {2, 3, 4, 6};      // pins for rows
 
-void setup()
-{
+
+// map 8 x 8 matrix to 64 LEDs
+const int matrix[8][8] = {
+  {0, 1, 2, 3, 4, 5, 6, 7},
+  {8, 9, 10, 11, 12, 13, 14, 15},
+  {16, 17, 18, 19, 20, 21, 22, 23},
+  {24, 25, 26, 27, 28, 29, 30, 31},
+  {32, 33, 34, 35, 36, 37, 38, 39},
+  {40, 41, 42, 43, 44, 45, 46, 47},
+  {48, 49, 50, 51, 52, 53, 54, 55},
+  {56, 57, 58, 59, 60, 61, 62, 63}
+};
+
+// boolean matrix to store the state of the board
+int board[BOARD_SIZE][BOARD_SIZE] = {
+  {1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+void setup(){
   // initialize serial communication
   Serial.begin(9600);
 
@@ -29,15 +53,39 @@ void setup()
   */
 }
 
-void loop()
-{
+void loop() {
   // constantly check for changes in the grid
   checkGrid();
   delay(1000);
 }
 
-void checkGrid()
-{
+/*
+  ///////////////////////////////functions/////////////////////////////////////
+*/ 
+void getBoardState() {
+  // loop through columns
+  for (int i = 0; i < sizeof(columnPins) / sizeof(int); i++) {
+    // set column to high
+    digitalWrite(columnPins[i], HIGH);
+
+    // loop through rows
+    for (int j = 0; j < sizeof(rowPins) / sizeof(int); j++) {
+      // if the row is high, set the corresponding board state to 1
+      if (digitalRead(rowPins[j]) == HIGH) {
+        board[i][j] = 1;
+      }
+      // if the row is low, set the corresponding board state to 0
+      else {
+        board[i][j] = 0;
+      }
+    }
+    // set column to low
+    digitalWrite(columnPins[i], LOW);
+  }
+}
+
+// check the grid for changes
+void checkGrid() {
   for (int i = 0; i < sizeof(columnPins) / sizeof(int); i++)
   {
     digitalWrite(columnPins[i], HIGH);
@@ -52,6 +100,7 @@ void checkGrid()
   }
 }
 
+// turn on LED at (x, y)
 void setLED(int x, int y)
 {
   Serial.print("LED is on at: ");
@@ -66,11 +115,11 @@ void setLED(int x, int y)
 // turn off a single LED
 void clearLED(int x, int y)
 {
-  Serial.print("LED is off at: ");
-  Serial.print(x);
-  Serial.print(',');
-  Serial.print(y);
-  Serial.println();
+  //  Serial.print("LED is off at: ");
+  //  Serial.print(x);
+  //  Serial.print(',');
+  //  Serial.print(y);
+  //  Serial.println();
   // strip.setPixelColor(matrix[x][y], strip.Color(0, 0, 0, 0));
   // strip.show();
 }
