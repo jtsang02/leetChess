07040fae985217ca621ce7069c35a2f52fe9077c
jtsang02 <jtsang02@student.ubc.ca>
2023-02-11 20:10:25 -0800

    2 x 2 wtf
    changed input to output and it works?!

--- Arduino/nxn_test/nxnTest/nxnTest.ino
+++ Arduino/nxn_test/nxnTest/nxnTest.ino
@@ -8,21 +8,21 @@
 #define BOARD_SIZE 2               // number of columns and rows in the board
 #define N_LEDS 8 * 8               // number of individual LEDs in one neopixel strip
 #define BOARD_PIN 12               // pin for the neopixel strip
-int columnPins[] = {8, 9, 11, 12}; // pins for columns
-int rowPins[] = {2, 3, 6, 7};      // pins for rows
+int columnPins[] = { 2, 3 };      // pins for columns
+int rowPins[] = { 8, 9 };      // pins for rows
 
 void setup()
 {
     // initialize serial communication
     Serial.begin(9600);
 
-    // initialize columns to output
+    // initialize columns to input
     for (int i = 0; i < sizeof(columnPins) / sizeof(int); i++)
-        pinMode(columnPins[i], OUTPUT);
+        pinMode(columnPins[i], INPUT);
 
-    // initialize rows to input
+    // initialize rows to output
     for (int i = 0; i < sizeof(rowPins) / sizeof(int); i++)
-        pinMode(rowPins[i], INPUT);
+        pinMode(rowPins[i], OUTPUT);
 
     /*
       initialize neopixel strip here // TODO
@@ -38,18 +38,29 @@ void loop()
 
 void checkGrid()
 {
-    for (int i = 0; i < sizeof(columnPins) / sizeof(int); i++)
+    for (int i = 0; i < sizeof(rowPins) / sizeof(int); i++)
     {
-        digitalWrite(columnPins[i], HIGH);
+        digitalWrite(rowPins[i], HIGH);
         for (int j = 0; j < sizeof(rowPins) / sizeof(int); j++)
         {
-            if (digitalRead(rowPins[j]) == HIGH)
+            if (digitalRead(columnPins[j]) == HIGH)
                 setLED(i, j); // TODO: update to setLED(x, y) function
-            else
-                clearLED(i, j); // TODO: update to clearLED(x, y) function
+            // else
+            //     clearLED(i, j); // TODO: update to clearLED(x, y) function
         }
-        digitalWrite(columnPins[i], LOW);
+        digitalWrite(rowPins[i], LOW);
     }
+    
+    // columns only
+
+    // digitalWrite(2, HIGH);
+    // // digitalWrite(3, LOW);
+    // if (digitalRead(6) == HIGH)
+    //   setLED(0, 0);
+    
+    // if (digitalRead(7) == HIGH)
+    //   setLED(0, 1);    
+
 }
 
 void setLED(int x, int y)
